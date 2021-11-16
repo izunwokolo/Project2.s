85a4a90ae14d579a83baaa4c7566012dbef8c551
izunwokolo <69478171+izunwokolo@users.noreply.github.com>
2021-11-15 20:54:39 -0500

    Update Project2.s

--- Project2.s
+++ Project2.s
@@ -2,3 +2,10 @@
 input: .space 1001 #Reserve 1001 bytes for input
 final_string: .space 4
 error: .asciiz "Not recognized"
+
+text #text section starts
+
+main: #Main Section Starts
+li $v0, 8 #Load Read input command
+la $a0, input #Mem Address from input is now destination MA
+syscall
